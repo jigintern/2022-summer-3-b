39d4604d7f7522e66d9146ad9904066ea1cd0402
hasegawa_itsuki <ii440092@icloud.com>
2022-08-29 17:06:18 +0900

    connect database

--- server.js
+++ server.js
@@ -1,12 +1,59 @@
-import { serve } from "https://deno.land/std@0.151.0/http/server.ts";
-import { serveDir } from "https://deno.land/std@0.151.0/http/file_server.ts";
+import {
+  serve
+} from "https://deno.land/std@0.138.0/http/server.ts";
+
+import {
+  serveDir
+} from "https://deno.land/std@0.138.0/http/file_server.ts";
+
+
+import {
+  initializeApp
+} from "https://www.gstatic.com/firebasejs/9.8.2/firebase-app.js";
+
+import {
+  getDatabase,
+  ref,
+  onValue,
+  push
+} from "https://www.gstatic.com/firebasejs/9.8.2/firebase-database.js";
+
+
 
 serve(async (req) => {
-  const pathname = new URL(req.url).pathname;
-  console.log(pathname);
+  const firebaseConfig = {
+    apiKey: "AIzaSyD3Ss5TXPzayE1K3BgdZRLCHugXE2yDcHg",
+    authDomain: "jig-jp-deno-2022.firebaseapp.com",
+    projectId: "deno-jig-jp-deno-2022",
+    storageBucket: "jig-jp-deno-2022.appspot.com",
+    messagingSenderId: "346542836062",
+    appId: "1:346542836062:web:1f1a78d1828ec4d438a7cf",
+    measurementId: "G-V822WJ6JXY",
+    databaseURL: "https://jig-jp-deno-2022-default-rtdb.firebaseio.com",
+  };
+  const app = initializeApp(firebaseConfig);
+  const database = getDatabase(app);
 
-  if (req.method === "GET" && pathname === "/welcome-message") {
-    return new Response("jigインターンへようこそ！");
+  const pathname = new URL(req.url).pathname;
+
+  if (req.method === "POST" && pathname === "/save") {
+    const now = new Date();
+    const requestJson = await req.json();
+    push(ref(database, 'saveData'), {
+      name: requestJson.name,
+      message: requestJson.message,
+      date: now.getMonth() + 1 + '月' + now.getDate() + '日' + now.getHours() + '時' + now.getMinutes() + '分'
+    });
+    return new Response('記録しました');
+  }
+
+  if (req.method === "GET" && pathname === "/saveData") {
+    const starCountRef = ref(database, 'saveData');
+    let saveDataLists;
+    onValue(starCountRef, (snapshot) => {
+      saveDataLists = snapshot.val();
+    });
+    return new Response(JSON.stringify(saveDataLists));
   }
 
   return serveDir(req, {
