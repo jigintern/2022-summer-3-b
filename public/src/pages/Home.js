9c79aeffb5968918194b770bc67c11f220ba9792
keke1008 <keke2222337@gmail.com>
2022-09-01 10:13:15 +0900

    ハビパワー表示の追加

--- public/src/pages/Home.js
+++ public/src/pages/Home.js
@@ -1,8 +1,10 @@
+import { Habipower } from "/src/components/Habipower.js";
+
 export const Home = {
   template: `
     <v-container fill-height>
 
-      <v-row justify="center" align-content="center">
+      <v-row justify="center" >
         <v-col align="center" cols="10">
           <v-text-field
             v-model="taskName"
@@ -19,12 +21,38 @@ export const Home = {
             @click="startMatching"
           >マッチング開始</v-btn>
         </v-col>
+
+      <v-col cols="12">
+        <v-divider />
+      </v-col>
+
+        <v-col cols="10">
+          <habipower :value="habipower" />
+        </v-col>
       </v-row>
 
     </v-container>
   `,
+
+  components: {
+    Habipower,
+  },
+
   data() {
-    return { taskName: "" };
+    return {
+      taskName: "",
+      habipower: null,
+    };
+  },
+
+  async created() {
+    // 仮のuser id
+    const userId = Math.floor(Math.random() * 10);
+
+    const body = JSON.stringify({ id: userId });
+    const res = await fetch("/api/habipower", { method: "POST", body });
+    const json = await res.json();
+    this.habipower = json.habipower;
   },
 
   methods: {
